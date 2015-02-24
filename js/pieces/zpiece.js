4a57a6582d07747e3bbe2b916131c97ba454adb3
Eric Hsu <erichsu7@gmail.com>
2015-02-24 01:40:02 -0800

    Can rotate z piece

---
+++ js/pieces/zpiece.js
@@ -0,0 +1,39 @@
+(function () {
+  var ZPiece = Tetris.ZPiece = function (board) {
+    Tetris.Piece.call(this, board);
+    this.blockCoords = [
+      [0, (this.board.cols / 2) - 2],
+      [0, (this.board.cols / 2) - 1],
+      [1, (this.board.cols / 2) - 1],
+      [1, (this.board.cols / 2)]
+    ];
+    this.position = "h";
+    this.generateBlocks();
+  };
+
+  ZPiece.inherits(Tetris.Piece);
+
+  ZPiece.prototype.rotate = function () {
+    var pivotBlock = this.blocks[2];
+    var pivotCoord = pivotBlock.coord;
+    if (this.position === "h") {
+      this.blockCoords = [
+        [pivotCoord.i - 1, pivotCoord.j + 1],
+        [pivotCoord.i, pivotCoord.j + 1],
+        [pivotCoord.i, pivotCoord.j],
+        [pivotCoord.i + 1, pivotCoord.j]
+      ]
+      this.position = "v";
+    } else {
+      this.blockCoords = [
+        [pivotCoord.i - 1, pivotCoord.j - 1],
+        [pivotCoord.i - 1, pivotCoord.j],
+        [pivotCoord.i, pivotCoord.j],
+        [pivotCoord.i, pivotCoord.j + 1]
+      ]
+      this.position = "h";
+    }
+    this.blocks = [];
+    this.generateBlocks();
+  };
+})();
